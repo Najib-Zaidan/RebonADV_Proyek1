e5a193ce8a64c4ee2df5d0badbd562b4886bc57b
Najib-Zaidan <najibzaidan2@gmail.com>
2026-04-09 22:46:36 +0700

    PHP, CRUD Peserta OT

--- php_dashboard_admin/proses_daftar.php
+++ php_dashboard_admin/proses_daftar.php
@@ -1,36 +1,18 @@
 <?php
 session_start();
-require "konek.php";
-
-// CEK LOGIN
-<<<<<<< HEAD:php_dashboard_admin/proses_daftar.php
-// if (!isset($_SESSION['username'])) {
-//     header("Location: login_user.html");
-//     exit;
-// }
-=======
-/*if (!isset($_SESSION['username'])) {
-    header("Location: login_user.html");
-    exit;
-}*/
->>>>>>> 6767cda9b697645547595e91a4af67c39bb92182:php_dashboard_admin/poses_daftar.php
+require 'konek.php';
 
 $nama = $_POST['nama'];
-$dd = $_POST['dd'];
-$mm = $_POST['mm'];
-$yyyy = $_POST['yyyy'];
 $alamat = $_POST['alamat'];
 $telepon = $_POST['telepon'];
-$penyakit = $_POST['penyakit'];
+$tglLahir = $_POST['tglLahir'];
 $detail = $_POST['detail'];
 
-$tanggal = $yyyy . "-" . $mm . "-" . $dd;
-var_dump($_POST);
-die();
-$query = "INSERT INTO pendaftaran 
-(nama, tanggal_lahir, alamat, telepon, penyakit, detail)
+
+$query = "INSERT INTO peserta 
+(nama, no_hp, tgl_lahir, alamat, riwayat)
 VALUES 
-('$nama','$tanggal','$alamat','$telepon','$penyakit','$detail')";
+('$nama','$telepon','$tglLahir','$alamat','$detail')";
 
 // var_dump($_POST);
 // die();
